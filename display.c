847d2033cc24f8a86a5b52fe52459887d4a7c84f
Diego-Moran <114490251+Moran2324@users.noreply.github.com>
2023-11-26 17:53:47 -0600

    Display Menu Added

--- display.c
+++ display.c
@@ -10,51 +10,15 @@
 
 
 static uint8_t M0_op1 [] = "1) Manual";
-static uint8_t M0_op2 [] = "2) Secuencia";
+static uint8_t M0_op2 [] = "2) Record";
 static uint8_t MManual_msg1 [] = "Manual Mode";
 static uint8_t MManual_msg2 [] = "press any";
 static uint8_t MManual_msg3 [] = "key";
-static uint8_t MSecuence_msg1 [] = "Secuence";
-static uint8_t Stopped_msg [] = "Stopped";
+static uint8_t MRecord_msg1 [] = "Press b1 to record";
 static uint8_t Playing_msg [] = "Playing";
 
 static uint8_t wdg_msg [] = "El sistema fallo";
 
-void DISPLAY_display_sound(SOUNDS_sound_t sound)
-{
-	switch (sound)
-	{
-	case kSounds_kickSound:
-		LCD_nokia_send_string(Kick_char);
-	break;
-	case kSounds_snareSound:
-		LCD_nokia_send_string(Snare_char);
-	break;
-	case kSounds_bongoSound:
-		LCD_nokia_send_string(Bongo_char);
-	break;
-	case kSounds_hitHatSound:
-		LCD_nokia_send_string(HitHat_char);
-	break;
-	case kSounds_guiroSound:
-		LCD_nokia_send_string(Guiro_char);
-	break;
-	case kSounds_rimShotSound:
-		LCD_nokia_send_string(RimShot_char);
-	break;
-	case kSounds_tamborSound:
-		LCD_nokia_send_string(Tambor_char);
-	break;
-	case kSounds_cynbalSound:
-		LCD_nokia_send_string(Cynbal_char);
-	break;
-	case kSounds_cowbellSound:
-		LCD_nokia_send_string(Cowbell_char);
-	break;
-	default:
-	break;
-	}
-}
 
 void DISPLAY_MenuSelec(Menu_t MenuSel)
 {
@@ -67,8 +31,8 @@ void DISPLAY_MenuSelec(Menu_t MenuSel)
 	case kDisplay_MManual:
 		DISPLAY_Menu_Manual();
 	break;
-	case kDisplay_MSecuence:
-		DISPLAY_Menu_Secuence();
+	case kDisplay_MRecord:
+		DISPLAY_Menu_Record();
 	break;
 	}
 }
@@ -91,27 +55,21 @@ void DISPLAY_Menu_Manual(void)
 	LCD_nokia_goto_xy(6, 4);
 	LCD_nokia_send_string(MManual_msg3);
 }
-void DISPLAY_Menu_Secuence(void)
+void DISPLAY_Menu_Record(void)
 {
 	LCD_nokia_clear();
-	LCD_nokia_goto_xy(0, 0);
-	LCD_nokia_send_string(MSecuence_msg1);
-	LCD_nokia_goto_xy(4, 5);
-	LCD_nokia_send_string(Stopped_msg);
-	LCD_nokia_goto_xy(0, 1);
+	LCD_nokia_goto_xy(0, 4);
+	LCD_nokia_send_string(MRecord_msg1);
 }
 
-void DISPLAY_Playing_msg(void)
+void DISPLAY_Recording_msg(void)
 {
+	LCD_nokia_clear();
 	LCD_nokia_goto_xy(4, 5);
 	LCD_nokia_send_string(Playing_msg);
+	/*FALTA PONER EL CONTADOR*/
 }
 
-void DISPLAY_InitialPic(void)
-{
-	LCD_nokia_clear();
-	LCD_nokia_bitmap(LCD_NOKIA_getPicPtr());
-}
 
 void DISPLAY_systemError(void)
 {
--- display.h
+++ display.h
@@ -9,21 +9,18 @@
 #define DISPLAY_H_
 
 #include <stdint.h>
-#include "sounds.h"
 
 typedef enum {
 	kDisplay_M0 = 0u,
 	kDisplay_MManual = 1u,
-	kDisplay_MSecuence = 2u
+	kDisplay_MRecord = 2u
 }Menu_t;
 
-void DISPLAY_display_sound(SOUNDS_sound_t sound);
 void DISPLAY_MenuSelec(Menu_t MenuSel);
 void DISPLAY_Menu0(void);
 void DISPLAY_Menu_Manual(void);
-void DISPLAY_Menu_Secuence(void);
-void DISPLAY_Playing_msg(void);
-void DISPLAY_InitialPic(void);
+void DISPLAY_Menu_Record(void);
+void DISPLAY_Recording_msg(void);
 void DISPLAY_systemError(void);
 
 #endif /* DISPLAY_H_ */
