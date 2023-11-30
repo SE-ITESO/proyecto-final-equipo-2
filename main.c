1ab15e198540b4ed45524656bb527addd959d7d7
Diego-Moran <114490251+Moran2324@users.noreply.github.com>
2023-11-30 12:42:01 -0600

    Display funs fixed

--- main.c
+++ main.c
@@ -1,12 +1,22 @@
 /*
- * Proyecto Final
- * Equipo 2
+ * Final porject
+ * Sistemas Embebidos Basados en Microcontroladores
+ *
+ * Authors:
  * José Miguel Castellanos Padilla
  * Diego Moran Vergara
  *
- * Sistemas Embebidos Basados en Microcontroladores 1
+ * Description:
+ *
+ * The system consists of an audio recorder that uses the SPI memory S25FL164K to store
+ * the audio samples read from the ADC module. The sampling rate is 30kHz.
+ *
+ * This system is also capable of sending the sampled audio directly to the DAC.
+ *
+ * The system uses convolution to add different effects to the audio
  *
  * */
+
 #include <stdint.h>
 #include "buttons.h"
 #include "dac.h"
@@ -15,9 +25,10 @@
 #include "LCD_nokia.h"
 #include "NVIC.h"
 #include "pit.h"
-#include "port.h"
 #include "SPI.h"
+#include "system_clock.h"
 #include "wdog.h"
+
 
 //State machine data type
 typedef struct{
@@ -36,6 +47,8 @@ const State_t FSM_Moore [4] =
 
 int main(void)
 {
+	SYSTEM_CLOCK_setup();
+	CLOCK_SetSimSafeDivs();
 
 	Menu_t current_state = kDisplay_M0;
 	uint8_t input = 0u;						//Machine input
@@ -54,7 +67,7 @@ int main(void)
 	NVIC_enable_interrupt_and_priotity(PORTD_IRQ, PRIORITY_3);
 	NVIC_global_enable_interrupts;
 
-	PORT_init();
+
 	BUTTONS_init();
 	BUTTONS_interrupt_enable();
 	PIT_init();
@@ -82,31 +95,31 @@ int main(void)
 		}
 
 		//If the Port D 3 switch has been depressed, go  to the real Time menu
-		if(GPIO_GetISR_StatusFlags(kGPIO_D) && (kDisplay_M0 == output))
+		if(GPIO_GetISR_StatusFlags(kGPIO_C) && (kDisplay_M0 == output))
 		{
 			input = kDisplay_MSetReTime;
-			GPIO_ClearISR_StatusFlags(kGPIO_D, PTD3);
+			GPIO_ClearISR_StatusFlags(kGPIO_C, PTC16);
 		}
 
 		//If the Port C 2 switch has been depressed, go  to the Recording menu
 		if(GPIO_GetISR_StatusFlags(kGPIO_C) && (kDisplay_M0 == output))
 		{
 			input = kDisplay_MPlay;
-			GPIO_ClearISR_StatusFlags(kGPIO_C, PTC2);
+			GPIO_ClearISR_StatusFlags(kGPIO_C, PTC17);
 
 		}
 
 		if(kDisplay_MSetReTime == output)
 		{
-			if(GPIO_GetISR_StatusFlags(kGPIO_D))
+			if(GPIO_GetISR_StatusFlags(kGPIO_C))
 			{
 				DISPLAY_Recording_msg();
-				GPIO_ClearISR_StatusFlags(kGPIO_D, PTD3);
+				GPIO_ClearISR_StatusFlags(kGPIO_C, PTC16);
 			}
 			if(GPIO_GetISR_StatusFlags(kGPIO_C))
 			{
 				/*Cambiar tiempo*/
-				GPIO_ClearISR_StatusFlags(kGPIO_C, PTC2);
+				GPIO_ClearISR_StatusFlags(kGPIO_C, PTC17);
 			}
 
 			if(GPIO_GetISR_StatusFlags(kGPIO_A))
@@ -133,20 +146,20 @@ int main(void)
 				GPIO_ClearISR_StatusFlags(kGPIO_A, PTA1);
 			}
 
-			if(GPIO_GetISR_StatusFlags(kGPIO_D))
+			if(GPIO_GetISR_StatusFlags(kGPIO_C))
 			{
 				DISPLAY_SoundEffects();
-				GPIO_ClearISR_StatusFlags(kGPIO_D, PTD3);
+				GPIO_ClearISR_StatusFlags(kGPIO_C, PTC16);
 			}
 
 			if(GPIO_GetISR_StatusFlags(kGPIO_C))
 			{
 				/*Start Play*/
-				GPIO_ClearISR_StatusFlags(kGPIO_C, PTC2);
+				GPIO_ClearISR_StatusFlags(kGPIO_C, PTC17);
 			}
 		}
 		//change the state of the machine depending on the input
 		current_state = FSM_Moore[current_state].next[input];
 	}
-	return 0 ;
+	return 0;
 }
